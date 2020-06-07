3df13d5d0c9d54e7d65e9b2b9f307f2e563be39c
wqf <wenqifeng97@163.com>
2020-06-07 10:02:13 +0800

    单项环形链表实现

---
+++ src/com/datastructure/linkedlist/CircleSingleLikedList.java
@@ -0,0 +1,52 @@
+package com.datastructure.linkedlist;
+
+/**
+ * @author Qifeng Wen
+ * @date 2020-06-07 上午9:45
+ */
+public class CircleSingleLikedList {
+    private Node first;
+
+    /**
+     * 添加节点，构成环形链表
+     *
+     * @param num
+     */
+    private void add(int num) {
+        if (num < 1) {
+            System.out.println("num的值必须大于0");
+            return;
+        }
+
+        first = new Node(1);
+        first.next = first;
+        Node curNode = first; //辅助指针，帮助构建环形链表
+        for (int i = 2; i <= num; i++) {
+            // 根据编号创建节点
+            Node node = new Node(i);
+            curNode.next = node;
+            node.next = first;
+            curNode = node;
+        }
+    }
+
+    public void printList() {
+        if (first == null) {
+            return;
+        }
+        Node curNode = first;
+        while (curNode != null) {
+            System.out.println(curNode);
+            curNode = curNode.next;
+            if (curNode == first)
+                break;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        CircleSingleLikedList circleSingleLikedList = new CircleSingleLikedList();
+        circleSingleLikedList.add(5);
+        circleSingleLikedList.printList();
+    }
+}
